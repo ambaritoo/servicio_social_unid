7b2789329165b53e062f9286e63916059b050c12
ambar <115164901+ambaritoo@users.noreply.github.com>
2024-06-13 12:38:24 -0600

    integración de shadcn

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
     <h1>Home Page</h1>
+    <Button>Click</Button>
     </>
   );
 }
