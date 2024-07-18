866fcf37edbc152472f927cfffbcbb77d279953b
Yatziri Moyao <moyaoyatziri@gmail.com>
2024-07-18 11:28:48 -0600

    Space blank while loading

--- components/protectdRoute/protectedRoute.js
+++ components/protectdRoute/protectedRoute.js
@@ -22,7 +22,7 @@ const ProtectedRoute = (WrappedComponent) => {
     }, [router, supabase])
 
     if (loading) {
-      return <div>Loading...</div>
+      return <div></div>
     }
 
     return <WrappedComponent {...props} />
